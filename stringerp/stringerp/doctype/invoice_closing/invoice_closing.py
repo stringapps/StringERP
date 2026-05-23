0e6c090f30c53bdc888f6b68f18faa2b6ffd5519
D-codE <d-code@D-codEs-MacBook-Pro.local>
2026-05-23 12:54:19 +0530

    Fix: Set use_multi_level_bom to 1 in create_stock_entry_from_bom_job function

--- stringerp/stringerp/doctype/invoice_closing/invoice_closing.py
+++ stringerp/stringerp/doctype/invoice_closing/invoice_closing.py
@@ -350,7 +350,7 @@ def create_stock_entry_from_bom_job(
         stock_entry.purpose = purpose
         stock_entry.from_bom = 1
         stock_entry.bom_no = bom_doc.name
-        stock_entry.use_multi_level_bom = bom_doc.with_operations
+        stock_entry.use_multi_level_bom = 1
         stock_entry.fg_completed_qty = bom.total_qty or 1
         stock_entry.set_posting_time = True
         posting_date, posting_time = get_posting_time_from_invoice(invoice_closing)
